78e1eb3d5ce35b99ba7ccd5fcb0d447b350fadd1
veeramharshitha <veeramharshitha233@gmail.com>
2022-08-25 18:14:47 +0530

    first commit

---
+++ jscript/homepage.js
@@ -0,0 +1,45 @@
+console.log("harshitha");
+
+
+document.getElementById("but").addEventListener("click",dropDown);
+
+
+function dropDown()
+{
+    document.getElementById("answer").classList.toggle("active");
+}
+document.getElementById("but1").addEventListener("click",dropDown1);
+
+
+function dropDown1()
+{
+    document.getElementById("answer1").classList.toggle("active1");
+}
+document.getElementById("but2").addEventListener("click",dropDown2);
+
+
+function dropDown2()
+{
+    document.getElementById("answer2").classList.toggle("active2");
+}
+document.getElementById("but3").addEventListener("click",dropDown3);
+
+
+function dropDown3()
+{
+    document.getElementById("answer3").classList.toggle("active3");
+}
+document.getElementById("but4").addEventListener("click",dropDown4);
+
+
+function dropDown4()
+{
+    document.getElementById("answer4").classList.toggle("active4");
+}
+document.getElementById("but5").addEventListener("click",dropDown5);
+
+
+function dropDown5()
+{
+    document.getElementById("answer5").classList.toggle("active5");
+}
